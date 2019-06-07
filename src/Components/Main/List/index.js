fbd735da55b55da93fc65070cb85e5317531c2db
Dharminder Dhanda <dharminderdhanda@nbnco.com.au>
2019-06-07 16:03:29 +1000

    List display updated

--- src/Components/Main/List/index.js
+++ src/Components/Main/List/index.js
@@ -5,85 +5,104 @@ import Toolbar from '@material-ui/core/Toolbar';
 import TextField from '@material-ui/core/TextField';
 import { MuiPickersUtilsProvider, TimePicker, DatePicker } from 'material-ui-pickers';
 
-import MenuItem from '@material-ui/core/MenuItem';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import Typography from '@material-ui/core/Typography';
+
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Grid from '@material-ui/core/Grid';
-import card from './card.css';
-import data from '../Dashboard/data.json';
 
-import { createStyles, makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent'; 
-
-import { withStyles } from '@material-ui/core/styles';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Button from '@material-ui/core/Button';
-import Divider from '@material-ui/core/Divider';
-
+import Chip from '@material-ui/core/Chip';
+import Moment from 'react-moment';
+import env from '../../../environment.json';
 
 export default class Details extends React.Component {
- 
-  constructor(props) {
-    super(props);
-    
-   
-    this.state = {
-	  activeStep:0,
-	  detailData:[]
-    };
 
-	axios({
-        method:'post',
-        url:'http://localhost:8080/api/getDetails',
-        data: {
-			UserId:localStorage.getItem('UserId'),
-			UserGroup:localStorage.getItem('UserGroup')
-        }
-      })
-      .then(response => {
-          console.log(response);
-          this.setState({ detailData:response.data });
-      })
-      .catch(function (error) {
-      console.log(error);
-      });
+	constructor(props) {
+		super(props);
+
+
+		this.state = {
+			activeStep: 0,
+			detailData: []
+		};
+
+		axios({
+			method: 'post',
+			url: 'http://localhost:8080/api/getDetails',
+			data: {
+				UserId: localStorage.getItem('UserId'),
+				UserGroup: localStorage.getItem('UserGroup')
+			}
+		})
+			.then(response => {
+				console.log(response);
+				this.setState({ detailData: response.data });
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
 
 	  
 
-    }
+	}
 
-  // export default function SimpleCard() {
-  //const classes = useStyles();
-  //const bull = <span className={classes.bullet}>•</span>;
+	// export default function SimpleCard() {
+	//const classes = useStyles();
+	//const bull = <span className={classes.bullet}>•</span>;
 
- render() {
+	render() {
 
-    return (
-      <div>
-      		<h1></h1>
-      		{this.state.detailData.map((detail,index)=>
-      			{
-      				return<div>
-      						<Card>
-      						<CardContent>
-        						<Typography class="card-title" color="textSecondary" gutterBottom> 				 
-      							<h1>{detail.title}-
-      							{detail.firstName}
-							     {detail.lastName}</h1></Typography><br />
-							       <Typography variant="body2" component="p">
-							        {detail.gender}<br />
+		return (
+			<div>
+				{this.state.detailData.map((detail, index) => {
+					return <div>
+						<Card className="talentRecord">
+							<CardHeader
+								avatar={
+									<Avatar>
+										{detail.firstName[0]}
+									</Avatar>
+								}
+								action={
+									<IconButton aria-label="Settings">
+										<MoreVertIcon />
+									</IconButton>
+								}
+								title={detail.firstName + " " + detail.lastName + " - " + detail.jobTitle}
+								subheader={detail.client + " - " + detail.workExpMax + "yrs of experience"}
+							/>
+
+							<CardContent>
+								<Typography variant="body2" component="p">
+									Presented to {detail.hiringManager} on  {detail.jobType} basis for {detail.jobCategory} with joining date <Moment format={env.timeFormat}>{detail.jobOpenedDate}</Moment>  at  {detail.address}, {detail.city} {detail.state} {detail.country}
+								</Typography>
+								<Typography variant="body2" component="p">
+									to {detail.client} for role of {detail.jobTitle}
+								</Typography>
+								<Typography variant="body2" component="p"> with commision amount : ${detail.commissionAmount}  {detail.offerStatus} on <Moment format={env.timeFormat}>{detail.commissionDate}</Moment>
+								</Typography>
+
+								<Typography color="textSecondary" gutterBottom>
+
+								</Typography>
+
+								<Typography class="card-title" color="textSecondary" gutterBottom>
+
+									{/* {detail.gender}<br />
 							        {detail.ethnicity}<br />
 							        {detail.citizenship}<br />
 							        {detail.workStatus}<br />
 							        {detail.primarySkill}<br />
-							        {detail.source}<br />
+							        <br />
 							        {detail.currentEmployer}<br />
 							        {detail.salaryMin}<br />
 							        {detail.salaryMax}<br />
@@ -91,21 +110,19 @@ export default class Details extends React.Component {
 							        {detail.city}<br />
 							        {detail.state}<br />
 							        {detail.country}<br />
-							        {detail.workExpMin}<br />
-							        {detail.workExpMax}<br />
 							        
 							        {detail.client}<br />
-							       {detail.hiringManager}<br />
+							        <br />
 							        {detail.jobTitle}<br />
-							        {detail.jobType}<br />
-							        {detail.jobCategory}<br />
+							       <br />
+							        <br />
 							       
 							        {detail.jobAddress}<br />
 							        {detail.jobCity}<br />
 							        {detail.jobState}<br />
 							        {detail.jobCountry}<br />
 							        
-							        {detail.jobOpenedDate}<br />
+							        <br />
 							        {detail.cvSubmissionDate}<br />
 							        {detail.offerStatus}<br />
 							        {detail.offerDate}<br />
@@ -136,18 +153,23 @@ export default class Details extends React.Component {
 							        {detail.revenueRealizationDate}<br />
 							        {detail.revenueAmount}<br />
 							        {detail.financialYear}<br />
-							        {detail.month}<br />
-							        </Typography>
-							        </CardContent>
-      								<CardActions>
-        							<Button size="small" variant="contained" color="secondary">Edit</Button>
-     								 </CardActions>
-    								</Card>
-      						</div>
-      			})}
+							        {detail.month}<br /> */}
+								</Typography>
+							</CardContent>
+							<CardActions>
+								{/* <Button size="small" variant="contained" color="secondary">Edit</Button> */}
+								<Chip label={detail.source} variant="outlined" />
+								<Chip label={detail.workStatus} color="primary" variant="outlined" />
+								<Chip label={detail.pipelineType} color="primary" variant="outlined" />
+								<Chip label={detail.recruiter} color="primary" variant="outlined" />
+								<Chip label={detail.team} color="primary" variant="outlined" />
+							</CardActions>
+						</Card>
+					</div>
+				})}
 
 
-      </div>
-    );
-  }
+			</div>
+		);
+	}
 };
